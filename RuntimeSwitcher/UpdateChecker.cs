61142bfec8e92a18b6ea572b24b2f89f3b16c60d
SERG <serg_ctg@mail.ru>
2026-07-21 13:40:19 +0300

    Prevent auto-update prompt from overwriting custom BodyWalkVR build with upstream GitLab DLLs

--- RuntimeSwitcher/UpdateChecker.cs
+++ RuntimeSwitcher/UpdateChecker.cs
@@ -19,26 +19,8 @@ namespace RuntimeSwitcher
 
         public static async Task<string> GetLatestHash()
         {
-            if (id != null)
-                return id;
-
-            try {
-                ServicePointManager.SecurityProtocol |= SecurityProtocolType.Tls12;
-                using (WebClient wc = new WebClient())
-                {
-                    wc.Headers.Add("User-Agent", "OpenComposite-RuntimeSwitcher");
-                    string json = await wc.DownloadStringTaskAsync("https://gitlab.com/api/v4/projects/znixian%2fOpenOVR/repository/commits/openxr?per_page=1");
-
-                    if (json.Substring(0, 1) != "[")
-                        json = "[" + json + "]";
-                    
-                    Commit commit = JsonConvert.DeserializeObject<List<Commit>>(json).First();
-
-                    return id = commit.id;
-                }
-            } catch {
-                return null;
-            }
+            await Task.Delay(1);
+            return null;
         }
     }
 }
